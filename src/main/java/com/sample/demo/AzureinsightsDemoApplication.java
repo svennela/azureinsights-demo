28ab50adfc11b1d836b4cb634896bf1d12c338f4
Sridhar Vennela <svennela@pivotal.io>
2020-09-02 17:17:18 -0700

    initial commit

---
+++ src/main/java/com/sample/demo/AzureinsightsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sample.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureinsightsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureinsightsDemoApplication.class, args);
+	}
+
+}
